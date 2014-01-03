415585d1d49ff5b935be7ab860fde50208dd5502
KD-Chang <ibelieveicaner@gmail.com>
2014-01-03 16:19:08 +0800

    Ch6 TCP

---
+++ Ch6/index.js
@@ -0,0 +1,43 @@
+/**
+Chapter 6 #TCP
+**/
+
+/*
+Module
+Trace the user count
+*/
+var count = 0, 
+	users = {};
+
+var net = require('net');
+
+var server = net.createServer(function(conn){
+	//handle the connection
+	console.log('\033[90m  new connection!\033[39m');
+	conn.write('\n>welcome to \033[92m node-chat\033[39m!'
+	 	+ '\n>' + count + ' ' +'other people are connect at this time'
+		+ '\n> please write your name and press enter : \n'
+	);
+	count++;
+	
+	conn.setEncoding('utf8');
+
+	conn.on('data', function(){
+		data.replace('\r\n', '');
+		console.log(data);
+	});
+
+	conn.on('close', function(){
+		count--;
+		delete users[nickname];
+	});
+
+});
+
+/*
+Listening
+*/
+server.listen(3000, function(){
+	console.log('\033[96m server listening on *:3000\033[39m');
+});
+

0045c0fe2d1eb5ae1794aec4926731d2c67d0064
KD-Chang <ibelieveicaner@gmail.com>
2014-01-05 20:23:11 +0800

    init ch9 express

--- Ch9/express-sample/app.js
+++ Ch9/express-sample/app.js
@@ -7,20 +7,37 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
+
 var path = require('path');
+//
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+/*
+In many environments (e.g. Heroku), and as a convention, 
+you can set the environment variable PORT to tell your web server what port to listen on.
+*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
+//it's a fast path to a favicon, typically one has static() lower in the stack so that routes can intercept if needed, however this is rarely needed for a favicon. favicon(__dirname + '/path/to/favicon.ico')
 app.use(express.logger('dev'));
+/*
+Request body parsing middleware supporting JSON, urlencoded, and multipart requests. 
+This middleware is simply a wrapper for the json(), urlencoded(), and multipart() middleware.
+*/
 app.use(express.json());
 app.use(express.urlencoded());
+
 app.use(express.methodOverride());
 app.use(app.router);
+/*
+The app.routes object houses all of the routes defined mapped by the associated HTTP verb. 
+This object may be used for introspection capabilities, for example Express uses this internally not only for routing but to provide default OPTIONS behaviour unless app.options() is used. 
+Your application or framework may also remove routes by simply by removing them from this object.
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -33,5 +50,4 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  console.log(process.env.PORT);
 });
